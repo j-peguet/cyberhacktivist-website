e1949e27e486f5bb4f9b1af54a0e2e0fd213a522
Jules Peguet <julespeguet.jp@gmail.com>
2021-11-23 15:31:01 +0100

    add build

---
+++ build/precache-manifest.3d903dbe824607d05d368899a02f62bd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deede904c4d3c56bc19093d4f2639435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f72f184afd9f1e48a9",
+    "url": "/static/css/main.54e4d357.chunk.css"
+  },
+  {
+    "revision": "0652c850df441a813c5f",
+    "url": "/static/js/2.6dfe2168.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/static/js/2.6dfe2168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f72f184afd9f1e48a9",
+    "url": "/static/js/main.0d5a2146.chunk.js"
+  },
+  {
+    "revision": "ea630fe81389fbceefbb",
+    "url": "/static/js/runtime-main.bf9f4589.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "34a7864e6d8da185e21c7090253efb1f",
+    "url": "/static/media/undraw_charts.34a7864e.png"
+  },
+  {
+    "revision": "ca19339a4242ce1a67b59735865ab70f",
+    "url": "/static/media/undraw_security.ca19339a.png"
+  },
+  {
+    "revision": "c8a503ac6629ffcf2ae22fd41bee0ec9",
+    "url": "/static/media/undraw_upvote.c8a503ac.png"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
